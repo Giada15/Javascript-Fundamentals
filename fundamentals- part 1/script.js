ef55b26d89247cc9c16645da233e2968d2fd1280
Giada <tascheriogiada@gmail.com>
2023-06-01 11:22:21 -0400

    Initial commit

---
+++ fundamentals- part 1/script.js
@@ -0,0 +1,65 @@
+// console.log(3 ** 3)
+
+// const weightMark = 85
+// const heightMark = 1.88
+
+// const weightJohn = 85
+// const heightJohn = 1.76
+
+// function bmiCalculator(height, weight) {
+//     return (weight / height ** 2).toFixed(2)
+// }
+
+// const bmiMark = bmiCalculator(heightMark, weightMark)
+// const bmiJohn = bmiCalculator(heightJohn, weightJohn)
+
+
+// --------------------IF/ELSE STATEMENT
+
+// const markHigherBMI = bmiMark > bmiJohn
+
+// console.log(bmiMark, bmiJohn, markHigherBMI)
+
+// if (bmiMark > bmiJohn) {
+//     console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})`)
+// } else {
+//     console.log(`John's BMI (${bmiJohn})) is higher than Mark's (${bmiMark})`)
+// }
+
+// const doplhinAverage = (97 + 112 + 101) / 3
+// const koalasAverage = (109 + 95 + 106) / 3
+
+// if (doplhinAverage === koalasAverage && doplhinAverage >= 100 && koalasAverage >= 100) {
+//     console.log("It's a draw")
+// } else if (doplhinAverage > koalasAverage && doplhinAverage >= 100) {
+//     console.log("Diplhin are the winners")
+// } else if (koalasAverage > doplhinAverage && koalasAverage >= 100) {
+//     console.log("Koalas are the winners")
+// }
+
+// -------------------THE SWITCH STATEMENT-------------
+
+// const day = "monday"
+
+// switch (day) {
+//     case "monday":
+//         console.log("The week started")
+//         break;
+//     case "tuesday":
+//     case "wednesday":
+//         console.log("Study from 8:00 to 12:00")
+//         break;
+//     default:
+//         console.log("Go to work")
+// }
+
+//----------------------TERNARY OPERARTOR---------------
+
+// const bill = 430
+// const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}`)
+
+
+
+
+
